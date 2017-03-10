a9fb47b85a16f9d713162c9548dd30e7e6c84f2d
zhichaoj <zhichaoshredder@gmail.com>
2017-03-10 11:27:57 -0500

    Add files via upload

--- code/HerbEnvironment.py
+++ code/HerbEnvironment.py
@@ -17,15 +17,15 @@ class HerbEnvironment(object):
             self.discrete_env.num_cells[idx] -= 1
 
         # add a table and move the robot into place
-        table = self.robot.GetEnv().ReadKinBodyXMLFile('models/objects/table.kinbody.xml')
+        self.table = self.robot.GetEnv().ReadKinBodyXMLFile('models/objects/table.kinbody.xml')
         
-        self.robot.GetEnv().Add(table)
+        self.robot.GetEnv().Add(self.table)
 
         table_pose = numpy.array([[ 0, 0, -1, 0.7], 
                                   [-1, 0,  0, 0], 
                                   [ 0, 1,  0, 0], 
                                   [ 0, 0,  0, 1]])
-        table.SetTransform(table_pose)
+        self.table.SetTransform(table_pose)
         
         # set the camera
         camera_pose = numpy.array([[ 0.3259757 ,  0.31990565, -0.88960678,  2.84039211],
@@ -37,6 +37,33 @@ class HerbEnvironment(object):
     def GetSuccessors(self, node_id):
 
         successors = []
+       
+        grid=self.discrete_env.NodeIdToGridCoord(node_id)
+        for i in range(0,self.discrete_env.dimension):
+            #newCoord=grid[:]
+            print "newCoord"
+            print grid
+
+            # Surbtaction neighbor
+            grid[i] = grid[i]+1
+            print "g1"
+            print grid
+            newNode = self.discrete_env.GridCoordToNodeId(grid)
+            print "node"
+            print newNode
+            if (not self.IsCollision(newNode )  and self.IsBoundary(newNode)):
+                successors.append(newNode )
+
+            # Addition Neighbor
+            grid[i] = grid[i]-2
+            print "g2"
+            print grid
+            print "node"
+            print newNode
+            newNode  = self.discrete_env.GridCoordToNodeId(grid)
+            if (not self.IsCollision(newNode)  and self.IsBoundary(newNode)):
+                successors.append(newNode )
+            grid[i]=grid[i]+1
 
         # TODO: Here you will implement a function that looks
         #  up the configuration associated with the particular node_id
@@ -52,16 +79,51 @@ class HerbEnvironment(object):
         # TODO: Here you will implement a function that 
         # computes the distance between the configurations given
         # by the two node ids
-       
+        line = NodeIdToConfiguration(end_id) - NodeIdToConfiguration(start_id)
+        dist = numpy.linalg.norm(line)
         return dist
 
     def ComputeHeuristicCost(self, start_id, goal_id):
         
         cost = 0
+        #start_config=self.discrete_env.NodeIdToConfigration(start_config)
+        #goal_config=self.discrete_env.NodeIdToConfigration(goal_config)
+
+
+        start_config=self.discrete_env.NodeIdToGridCoord(start_id)
+        goal_config=self.discrete_env.NodeIdToGridCoord(goal_id)
+
+        for i in range(len(start_id)):
+            cost=cost+abs(start_config(i)-goal_config(i))
 
         # TODO: Here you will implement a function that 
         # computes the heuristic cost between the configurations
         # given by the two node ids
         
         return cost
+
+    def IsBoundary(self,coord):
+        conf=self.discrete_env.NodeIdToConfiguration(coord)
+        for i in range(len(conf)):
+            if conf[i]<self.lower_limits[i] or conf[i]>self.upper_limits[i] :
+                return False
+        return True
+
+
+
+    def IsCollision(self,coord):
+        conf=self.discrete_env.NodeIdToConfiguration(coord)
+        self.robot.SetActiveDOFValues(conf)
+
+        if self.robot.GetEnv().CheckCollision(self.robot,self.table) or self.robot.GetEnv().CheckCollision(self.robot,self.robot):
+            return True
+        else:
+            return False
+          
+
+
+
+
+
+
 
